b64f90684ae8f62b42f5bb670e751193f62c8d9a
stephanialucia <94402180+stephanialucia@users.noreply.github.com>
2021-11-21 00:22:04 -0400

    Add files via upload

---
+++ static/carrito.js
@@ -0,0 +1,8 @@
+(()=> {
+ fetch('http://localhost:3000/dolar').then(data => data.json()).then(data => {
+    let dolar = data.dolar
+    console.log(parseFloat(dolar).toFixed(2))
+    alert(`El precio del dolar es: ${parseFloat(dolar).toFixed(2)}`)
+ }).catch(e => console.log(e))
+
+})();
